6c4a3109c17f4996b223aabf3e6e192b5a90f896
landvibe.podo <landvibe@kakaopage.com>
2019-04-20 21:31:52 +0900

    비동기 렌더링 테스트 코드 제거

--- 10-chapter/1-suspense/src/index.js
+++ 10-chapter/1-suspense/src/index.js
@@ -2,8 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// 동기 렌더링
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// 비동기 렌더링
-// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
