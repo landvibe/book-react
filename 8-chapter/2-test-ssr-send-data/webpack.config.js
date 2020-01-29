2e4bb541277b9044752ebd23a2836262f82ea10f
landvibe.podo <landvibe@kakaopage.com>
2020-01-29 23:31:39 +0900

    8장에서 clean-webpack-plugin 제거

--- 8-chapter/2-test-ssr-send-data/webpack.config.js
+++ 8-chapter/2-test-ssr-send-data/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -23,7 +22,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './template/index.html',
     }),
--- 8-chapter/3-test-ssr-image-module/webpack.config.js
+++ 8-chapter/3-test-ssr-image-module/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 
@@ -52,7 +51,6 @@ function getConfig(isServer) {
     plugins: isServer
       ? []
       : [
-          new CleanWebpackPlugin(),
           new HtmlWebpackPlugin({
             template: './template/index.html',
           }),
